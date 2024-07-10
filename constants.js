24b5b4cd6ba57e40392077cc9985432017147279
Unstream <143244075+wont-stream@users.noreply.github.com>
2024-07-10 03:53:36 -0400

    Update constants.js

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_223.0_57230.ipa";
+Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/stable/Discord_226.1.ipa";
 export { Shell, Colors, Divider, States, Constants };
